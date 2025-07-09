95db954337acaae7aedc53f8df941d5fc1562dec
shailesh <shailesh811cn@gmail.com>
2025-07-09 12:04:04 +0530

    changes to svelte config for cloudflare adapter

--- Frontend/svelte.config.js
+++ Frontend/svelte.config.js
@@ -1,4 +1,4 @@
-import adapter from '@sveltejs/adapter-vercel';
+import adapter from '@sveltejs/adapter-auto';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 import { fileURLToPath } from 'node:url';
 import { dirname, resolve } from 'node:path';
