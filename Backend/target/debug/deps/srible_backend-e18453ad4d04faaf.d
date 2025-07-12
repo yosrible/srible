c90cef3a19a4ff8a80b5050d7353ac35e5e4a13c
kazedevs <businessrajkumardas@gmail.com>
2025-07-12 16:46:47 +0530

    feat: add basic Rust backend with hello world

---
+++ Backend/target/debug/deps/srible_backend-e18453ad4d04faaf.d
@@ -0,0 +1,5 @@
+/Users/kaze/Desktop/codeprv/srible/Backend/target/debug/deps/srible_backend-e18453ad4d04faaf: main.rs
+
+/Users/kaze/Desktop/codeprv/srible/Backend/target/debug/deps/srible_backend-e18453ad4d04faaf.d: main.rs
+
+main.rs:
---
+++ Backend/target/debug/srible-backend.d
@@ -0,0 +1,1 @@
+/Users/kaze/Desktop/codeprv/srible/Backend/target/debug/srible-backend: /Users/kaze/Desktop/codeprv/srible/Backend/main.rs
